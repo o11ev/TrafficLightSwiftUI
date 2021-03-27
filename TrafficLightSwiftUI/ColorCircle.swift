c06dfa762abd62030f052a25bc3256424545ccc7
Olga Dragunova <75809668+o11ev@users.noreply.github.com>
2021-03-27 23:16:34 +0300

    some edits with elements

--- TrafficLightSwiftUI/ColorCircle.swift
+++ TrafficLightSwiftUI/ColorCircle.swift
@@ -9,16 +9,18 @@ import SwiftUI
 
 struct ColorCircle: View {
     let color: Color
+    var opacity = 0.0
     var body: some View {
         Circle()
             .foregroundColor(color)
             .frame(width: 100, height: 100, alignment: .center)
             .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .opacity(opacity)
     }
 }
 
 struct ColorCircle_Previews: PreviewProvider {
     static var previews: some View {
-        ColorCircle(color: .red)
+        ColorCircle(color: .red, opacity: 0.5)
     }
 }
--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -8,18 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        
         ZStack {
             Color.black
                 .edgesIgnoringSafeArea(.all)
             VStack(spacing: 200.0) {
                 VStack(spacing: 20.0) {
-                    ColorCircle(color: .red)
-                    ColorCircle(color: .yellow)
-                    ColorCircle(color: .green)
+                    ColorCircle(color: .red, opacity: 1.0)
+                    ColorCircle(color: .yellow, opacity: 0.5)
+                    ColorCircle(color: .green, opacity: 0.5)
                 }
-                NextButton()
+//                NextButton()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Button")
+                        .font(.title)
+                        .foregroundColor(.white)
+                })
             }
         }
     }
