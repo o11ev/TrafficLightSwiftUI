1e8200a87f6309ee3e54732a8552c66672e07381
Olga Dragunova <75809668+o11ev@users.noreply.github.com>
2021-03-27 22:18:16 +0300

    work in progress

--- TrafficLightSwiftUI/ColorCircle.swift
+++ TrafficLightSwiftUI/ColorCircle.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct ColorCircle: View {
+    let color: Color
     var body: some View {
         Circle()
+            .foregroundColor(color)
+            .frame(width: 150, height: 150, alignment: .center)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
     }
 }
 
 struct ColorCircle_Previews: PreviewProvider {
     static var previews: some View {
-        ColorCircle()
+        ColorCircle(color: .red)
     }
 }
--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ColorCircle(color: .red)
+            ColorCircle(color: .yellow)
+            ColorCircle(color: .green)
+        }
+        .background(Color.black)
     }
 }
 
