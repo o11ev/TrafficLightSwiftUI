1c121060c8ebb78cea0fd3e2f887712efa682081
Olga Dragunova <75809668+o11ev@users.noreply.github.com>
2021-03-27 21:42:52 +0300

    Starting

---
+++ TrafficLightSwiftUI/ColorCircle.swift
@@ -0,0 +1,20 @@
+//
+//  ColorCircle.swift
+//  TrafficLightSwiftUI
+//
+//  Created by o11ev on 27.03.2021.
+//
+
+import SwiftUI
+
+struct ColorCircle: View {
+    var body: some View {
+        Circle()
+    }
+}
+
+struct ColorCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCircle()
+    }
+}
