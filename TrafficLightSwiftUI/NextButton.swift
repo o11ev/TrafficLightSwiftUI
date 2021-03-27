d9f2241fae1401cbb1f6944d2bd50c0027a84fac
Olga Dragunova <75809668+o11ev@users.noreply.github.com>
2021-03-27 22:38:12 +0300

    created button

---
+++ TrafficLightSwiftUI/NextButton.swift
@@ -0,0 +1,32 @@
+//
+//  NextButton.swift
+//  TrafficLightSwiftUI
+//
+//  Created by o11ev on 27.03.2021.
+//
+
+import SwiftUI
+
+struct NextButton: View {
+    var body: some View {
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            Text("NEXT")
+                .padding(.horizontal, 30.0)
+        })
+        .font(.largeTitle)
+        .padding()
+        .background(Color.blue)
+        .foregroundColor(.white)
+        .cornerRadius(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.red, lineWidth: 5)
+        )
+    }
+}
+
+struct NextButton_Previews: PreviewProvider {
+    static var previews: some View {
+        NextButton()
+    }
+}
